5d66d943035c3c909fbc3ecb451ebb8ec18a50b1
Codeamon <CodeamonCat@gmail.com>
2024-01-30 19:16:16 +0800

    change code style based on google

--- 1_Two_Sum/1_Two_Sum.py
+++ 1_Two_Sum/1_Two_Sum.py
@@ -1,16 +1,19 @@
-__author__= "CodeamonCat"
+__author__ = "CodeamonCat"
 __email__ = "CodeamonCat@gmail.com"
 
 # from typing import List
 
+
 class Solution:
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsDict = dict()   # key: nums, value: nums_index
+        numsDict = dict()  # key: nums, value: nums_index
         for idx, val in enumerate(nums):
-            if numsDict.__contains__(target-val):
-                return [numsDict[target-val], idx]
+            if numsDict.__contains__(target - val):
+                return [numsDict[target - val], idx]
             else:
                 numsDict[val] = idx
+
 
 """
 if __name__ == '__main__':
