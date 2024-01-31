fcc556d8ee264c08eb30b29ac4069ac0c75c9fda
Codeamon <CodeamonCat@gmail.com>
2024-01-31 16:39:28 +0800

    update the variables

--- 1_Two_Sum/1_Two_Sum.py
+++ 1_Two_Sum/1_Two_Sum.py
@@ -7,12 +7,13 @@ __email__ = "CodeamonCat@gmail.com"
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsDict = dict()  # key: nums, value: nums_index
-        for idx, val in enumerate(nums):
-            if numsDict.__contains__(target - val):
-                return [numsDict[target - val], idx]
+        nums_dict = dict()  # key: nums, value: nums_index
+        for index, value in enumerate(nums):
+            if nums_dict.__contains__(target - value):
+                return [nums_dict[target - value], index]
             else:
-                numsDict[val] = idx
+                nums_dict[value] = index
+        return [-1, -1]
 
 
 """
