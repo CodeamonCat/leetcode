228b3966d74e20b7892bfca9c2fbffc6f9d3284c
Codeamon <CodeamonCat@gmail.com>
2026-03-24 15:11:38 +0800

    [update] 274_H_Index.py

--- 274_H_Index/274_H_Index.py
+++ 274_H_Index/274_H_Index.py
@@ -5,8 +5,14 @@ __email__ = "CodeamonCat@gmail.com"
 class Solution:
 
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
-        for idx, val in enumerate(citations):
-            if not (val > idx):
-                return idx
-        return len(citations)
+        c_len = len(citations)
+        c_list = [0] * (c_len + 1)
+        for c in citations:
+            c_list[min(c, c_len)] += 1
+
+        h_val = 0
+        for i in range(c_len, 0, -1):
+            h_val += c_list[i]
+            if h_val >= i:
+                return i
+        return 0
